1835a9c4a3e6f27a6b0b736dc819af1655291730
Mikel Azkolain <azkotoki@gmail.com>
2011-03-19 11:39:27 +0100

    spotify: Updated manager and fixed session initializations.

--- src/spotify/__init__.py
+++ src/spotify/__init__.py
@@ -10,37 +10,49 @@ class SpotifySessionManager:
     _session = None
     
     def __init__(self, **kwargs):
-        _session = session.Session(self, kwargs)
-    
-    def logged_in(self):
-        pass
-    
-    def logged_out(self):
-        pass
-    
-    def metadata_updated(self):
-        pass
-    
-    def connection_error(self):
-        pass
-    
-    def message_to_user(self):
-        pass
-    
-    def notify_main_thread(self):
-        pass
+        _session = session.Session(self, **kwargs)
     
     def search(self, query):
+        pass
+    
+    def logged_in(self, session, error):
+        pass
+    
+    def logged_out(self, session):
+        pass
+    
+    def metadata_updated(self, session):
+        pass
+    
+    def connection_error(self, session, error):
+        pass
+    
+    def message_to_user(self, session, message):
+        pass
+    
+    def notify_main_thread(self, session):
         pass
     
     def music_delivery(self, format, frames, num_frames):
         pass
     
-    def play_token_lost(self):
+    def play_token_lost(self, session):
         pass
     
-    def log_message(self, message):
+    def log_message(self, session, message):
         pass
     
-    def end_of_track(self):
+    def end_of_track(self, session, error):
+        pass
+    
+    def userinfo_updated(self, session):
+        pass
+    
+    def start_playback(self, session):
+        pass
+    
+    def stop_playback(self, session):
+        pass
+    
+    def get_audio_buffer_stats(self, session, stats):
         pass
--- src/spotify/session.py
+++ src/spotify/session.py
@@ -13,37 +13,53 @@ from spotify import user
 
 #classes
 class Session:
-    api_version = 6
+    api_version = 7
+    _manager = None
     
-    def __init__(self, manager, cache_location=None, settings_location=None, app_key=None, user_agent=None):
+    def __init__(self, manager, cache_location="", settings_location="", app_key=None, user_agent=None):
+        self._manager = manager
+        
         #prepare callbacks
         callbacks = _session.callbacks(
-            _session.cb_logged_in(manager.logged_in),
-            _session.cb_logged_out(manager.logged_out),
-            _session.cb_metadata_updated(manager.metadata_updated),
-            _session.cb_connection_error(manager.connection_error),
-            _session.cb_message_to_user(manager.message_to_user),
-            _session.cb_notify_main_thread(manager.notify_main_thread),
-            _session.cb_music_delivery(manager.music_delivery),
-            _session.cb_play_token_lost(manager.play_token_lost),
-            _session.cb_log_message(manager.log_message),
-            _session.cb_end_of_track(manager.end_of_track),
+            _session.cb_logged_in(self._logged_in),
+            _session.cb_logged_out(self._logged_out),
+            _session.cb_metadata_updated(self._metadata_updated),
+            _session.cb_connection_error(self._connection_error),
+            _session.cb_message_to_user(self._message_to_user),
+            _session.cb_notify_main_thread(self._notify_main_thread),
+            _session.cb_music_delivery(self._music_delivery),
+            _session.cb_play_token_lost(self._play_token_lost),
+            _session.cb_log_message(self._log_message),
+            _session.cb_end_of_track(self._end_of_track),
+            _session.cb_streaming_error(self._streaming_error),
+            _session.cb_userinfo_updated(self._userinfo_updated),
+            _session.cb_start_playback(self._start_playback),
+            _session.cb_stop_playback(self._stop_playback),
+            _session.cb_get_audio_buffer_stats(self._get_audio_buffer_stats),
         )
+        
+        #app key conversion
+        appkey_type = ctypes.c_byte * len(app_key)
+        appkey_c = appkey_type.from_buffer(bytearray(app_key))
         
         #initialize app config
         config = _session.config(
             self.api_version,
             cache_location,
             settings_location,
-            app_key,
-            ctypes.sizeof(app_key),
+            appkey_c,
+            ctypes.sizeof(appkey_c),
             user_agent,
-            callbacks,
-            None
+            ctypes.pointer(callbacks),
+            ctypes.c_void_p(),
+            1,
+            0,
+            1,
         )
         
-        sess_p = ctypes.c_void_p
-        _session.create(config, sess_p)
+        sess_p = ctypes.c_void_p()
+        err = _session.create(ctypes.byref(config), ctypes.byref(sess_p))
+        
         self._session = sess_p
     
     
@@ -80,6 +96,9 @@ class Session:
     def userdata(self):
         return _session.userdata(self._session)
     
+    def set_cache_size(self, size):
+        _session.set_cache_size(size)
+    
     def process_events(self):
         next_timeout = 0
         _session.process_events(self._session, next_timeout)
@@ -99,3 +118,49 @@ class Session:
     
     def playlistcontainer(self):
         pass
+    
+    #Callback proxies
+    def _logged_in(self, session, error):
+        self._manager.logged_in(self, error)
+    
+    def _logged_out(self, session):
+        self._manager.logged_out(self)
+    
+    def _metadata_updated(self, session):
+        self._manager.metadata_updated(self)
+    
+    def _connection_error(self, session, error):
+        self._manager.connection_error(self, error)
+    
+    def _message_to_user(self, session, message):
+        self._manager.message_to_user(self, message)
+    
+    def _notify_main_thread(self, session):
+        self._manager.notify_main_thread(self)
+    
+    def _music_delivery(self, session, format, frames, num_frames):
+        self._manager.music_delivery(self, format, frames, num_frames)
+    
+    def _play_token_lost(self, session):
+        self._manager.play_token_lost(self)
+    
+    def _log_message(self, session, data):
+        self._manager.log_message(self, data)
+    
+    def _end_of_track(self, session, error):
+        self._manager.end_of_track(self, error)
+    
+    def _streaming_error(self, session, error):
+        self._manager.streaming_error(self, error)
+    
+    def _userinfo_updated(self, session):
+        self._manager.userinfo_updated(self)
+    
+    def _start_playback(self, session):
+        self._manager.start_playback(self)
+    
+    def _stop_playback(self, session):
+        self._manager.stop_playback(self)
+    
+    def _get_audio_buffer_stats(self, session, stats):
+        self._manager.get_audio_buffer_stats(self, stats)
