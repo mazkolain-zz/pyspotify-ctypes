649c7f620846c70cd893352eca2b11974cb15c5f
Mikel Azkolain <azkotoki@gmail.com>
2011-04-19 00:14:12 +0200

    spotify: utils: Added the synchronize decorator.

---
+++ src/spotify/utils/decorators.py
@@ -0,0 +1,34 @@
+'''
+Created on 18/04/2011
+
+@author: mikel
+'''
+import threading
+import types
+
+
+class synchronized(object):
+    _func = None
+    _lock = threading.RLock()
+    
+    
+    def __init__(self, func):
+        print func
+        self._func = func
+    
+    
+    def __call__(self, *args, **kwargs):
+        self._lock.acquire()
+        result = self._func(*args, **kwargs)
+        self._lock.release()
+        return result
+    
+    
+    def __get__(self, obj, ownerClass=None):
+        return types.MethodType(self, obj)
+    
+    
+    @staticmethod
+    def get_lock():
+        return synchronized._lock
+        
