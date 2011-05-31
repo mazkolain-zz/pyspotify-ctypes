20ef47cbd50289a2d85b0cf73696e58aeebfb7d0
Mikel Azkolain <azkotoki@gmail.com>
2011-06-01 01:04:10 +0200

    _spotify: added fallback code if ctypes fails to locate libspotify.

--- src/_spotify/__init__.py
+++ src/_spotify/__init__.py
@@ -1,8 +1,4 @@
-import os
-import ctypes
-
-#For arch calculations
-import struct
+import struct, os, ctypes, sys 
 
 
 #Module index
@@ -22,10 +18,12 @@ voidp_size = struct.calcsize("P") * 8
 if os.name == "nt" and voidp_size == 32:
     loader = ctypes.windll
     callback = ctypes.WINFUNCTYPE
+    filename = "libspotify.dll"
 
 elif os.name == "posix" and voidp_size in [32,64]:
     loader = ctypes.cdll
     callback = ctypes.CFUNCTYPE
+    filename = "libspotify.so"
     
 else:
     raise OSError(
@@ -34,8 +32,25 @@ else:
     )
 
 
-#Global libspotify instance
-libspotify = loader.libspotify
+
+def __load_libspotify(loader, filename):
+    for path in sys.path:
+        full_path = os.path.join(path, filename)
+        if os.path.isfile(full_path):
+            return loader.LoadLibrary(full_path)
+    
+    raise OSError("Unable to find libspotify")
+
+
+
+#Global libspotify instance, let ctypes find it
+try:
+    libspotify = loader.libspotify
+
+#Bad luck, let's do a quirk
+except OSError:
+    libspotify = __load_libspotify(loader, filename)
+
 
 
 #structure definitions
@@ -47,6 +62,7 @@ class subscribers(ctypes.Structure):
 
 class audio_buffer_stats(ctypes.Structure):
     pass
+
 
 
 #completion of types
@@ -65,6 +81,7 @@ audio_buffer_stats._fields_ = [
     ("samples", ctypes.c_int),
     ("stutter", ctypes.c_int),
 ]
+
 
 
 #Function declarations
