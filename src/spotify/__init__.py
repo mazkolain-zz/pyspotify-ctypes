d0cd5ad28ee870956a9f8015cff3d22d6b3b2350
Mikel Azkolain <azkotoki@gmail.com>
2012-02-04 15:45:32 +0100

    Spotify: Added the ErrorType enum.

--- src/spotify/__init__.py
+++ src/spotify/__init__.py
@@ -42,6 +42,43 @@ class UnknownCallbackError(LibSpotifyError):
 
 
 
+class ErrorType:
+    Ok = 0
+    BadApiVersion = 1
+    ApiInitializationFailed = 2
+    TrackNotPlayable = 3
+    BadApplicationKey = 5
+    BadUsernameOrPassword = 6
+    UserBanned = 7
+    UnableToContactServer = 8
+    ClientTooOld = 9
+    OtherPermanent = 10
+    BadUserAgent = 11
+    MissingCallback = 12
+    InvalidIndata = 13
+    IndexOutOfRange = 14
+    UserNeedsPremium = 15
+    OtherTransient = 16
+    IsLoading = 17
+    NoStreamAvailable = 18
+    PermissionDenied = 19
+    InboxIsFull = 20
+    NoCache = 21
+    NoSuchUser = 22
+    NoCredentials = 23
+    NetworkDisabled = 24
+    InvalidDeviceId = 25
+    CantOpenTraceFile = 26
+    ApplicationBanned = 27
+    OfflineTooManyTracks = 31
+    OfflineDiskCache = 32
+    OfflineExpired = 33
+    OfflineNotAllowed = 34
+    OfflineLicenseLost = 35
+    OfflineLicenseError = 36
+
+
+
 class SampleType:
     Int16NativeEndian = 0
 
