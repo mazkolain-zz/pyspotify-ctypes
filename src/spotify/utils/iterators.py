1085b71f8e5f492bbf52f2f577238d8c01261edd
Mikel Azkolain <azkotoki@gmail.com>
2012-02-27 20:55:11 +0100

    Spotify: Utils: Implement __getitem__ in CallbackIterator.

--- src/spotify/utils/iterators.py
+++ src/spotify/utils/iterators.py
@@ -30,3 +30,18 @@ class CallbackIterator:
             return item
         else:
             raise StopIteration
+    
+    
+    def __getitem__(self, index):
+        if not isinstance(index, int):
+            raise TypeError('List indexes must be integers')
+        
+        if index < 0:
+            real_index = self.__count_method() + index
+        else:
+            real_index = index
+        
+        if real_index < 0 or real_index >= self.__count_method():
+            raise IndexError('List index out of range')
+        
+        return self.__item_method(real_index)
