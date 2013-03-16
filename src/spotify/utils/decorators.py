5a8961e13f29ffd8d541474beb9df5777a83be97
Mikel Azkolain <azkotoki@gmail.com>
2013-03-16 20:27:29 +0100

    Remove background task management stuff.

--- src/spotify/utils/decorators.py
+++ src/spotify/utils/decorators.py
@@ -35,28 +35,3 @@ class synchronized(object):
     @staticmethod
     def get_lock():
         return synchronized._lock
-
-
-
-def run_in_thread(func=None, threads_per_class=10, single_instance=False):
-    if callable(func):
-        decorator = run_in_thread()
-        return decorator(func)
-    
-    class decorator_class(object):
-        __func = None
-        
-        
-        def __init__(self, func):
-            self.__func = func
-        
-        
-        def __call__(self, *args, **kwargs):
-            tm = threads.TaskManager(threads_per_class, single_instance)
-            tm.add(self.__func, *args, **kwargs)
-        
-        
-        def __get__(self, obj, type=None):
-            return self.__class__(self.__func.__get__(obj, type))
-    
-    return decorator_class
